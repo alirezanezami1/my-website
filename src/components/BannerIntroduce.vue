b574c9bcd8dd6b8779189c972fb121a594403c86
Alireza <nezamialireza1022@gmail.com>
2026-07-15 13:49:42 +0330

    add bannerintroduce

---
+++ src/components/BannerIntroduce.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="w-full hidden lg:block">
+    <img src="../assets/images/my-banner.jpg" alt="banner" class="rounded-xl object-cover" />
+  </div>
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,12 @@
 <script setup>
+import BannerIntroduce from '@/components/BannerIntroduce.vue'
 </script>
 
 <template>
- <div></div>
+  <div
+    class="w-full mx-auto max-w-7xl bg-red-300 lg:px-10 lg:pt-9 lg:pb-11 flex flex-col justify-start items-center"
+  >
+    <!-- //// banner  -->
+    <BannerIntroduce />
+  </div>
 </template>
