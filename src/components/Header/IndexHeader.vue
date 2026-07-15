e7321349c736ce733238b2e2f995ac87b7c13e86
Alireza <nezamialireza1022@gmail.com>
2026-07-15 19:40:09 +0330

    install vueUse

--- src/components/Header/IndexHeader.vue
+++ src/components/Header/IndexHeader.vue
@@ -1,11 +1,19 @@
 <script setup>
+import { computed } from 'vue'
+import { useWindowSize } from '@vueuse/core'
+
 import DesktopHeader from './DesktopHeader.vue'
 import MobileHeader from './MobileHeader.vue'
+
+const { width } = useWindowSize()
+
+const currentComponent = computed(() => {
+  return width.value >= 1024 ? DesktopHeader : MobileHeader
+})
 </script>
 
 <template>
   <div class="flex w-full">
-    <MobileHeader class="block lg:hidden" />
-    <DesktopHeader class="hidden lg:block" />
+    <component :is="currentComponent"></component>
   </div>
 </template>
