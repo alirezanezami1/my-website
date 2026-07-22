c474350b6f9a9fd239984eb1eba2a2b58774e1fb
Alireza <nezamialireza1022@gmail.com>
2026-07-22 21:55:01 +0330

    edit style

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -64,7 +64,7 @@ onMounted(async () => {
 
 <template>
   <div
-    class="w-full mx-auto max-w-7xl bg-red-300 gap-4 lg:gap-10 lg:px-10 lg:pt-9 lg:pb-11 flex flex-col justify-start items-center"
+    class="w-full mx-auto max-w-7xl gap-4 lg:gap-10 lg:px-10 lg:pt-9 lg:pb-11 flex flex-col justify-start items-center"
   >
     <div class="w-full flex flex-col justify-start items-center gap-0 lg:gap-[13px]">
       <!-- //// banner  -->
