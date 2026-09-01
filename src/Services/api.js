6c29618594b89cf901710c70591d8b230e4e7bf5
Alireza <nezamialireza1022@gmail.com>
2026-09-01 23:07:42 +0330

    add toastification for call api

--- src/Services/api.js
+++ src/Services/api.js
@@ -1,4 +1,7 @@
 import axios from 'axios'
+import { useToast } from 'vue-toastification'
+
+const toast = useToast()
 
 const api = axios.create({
   baseURL: 'http://localhost:8080/resume-backend/wp-json/wp/v2',
@@ -10,7 +13,16 @@ export const getProfileData = async () => {
     const response = await api.get('/pages/26')
     return response.data.acf
   } catch (error) {
-    console.error('Error fetching profile:', error)
+    toast.error('در دریافت اطلاعات مشکلی پیش آمد. لطفاً بعداً دوباره تلاش کنید.')
+  }
+}
+
+export const getProjectsData = async () => {
+  try {
+    const response = await api.get('/projects')
+    return response.data
+  } catch (error) {
+    toast.error('در دریافت اطلاعات مشکلی پیش آمد. لطفاً بعداً دوباره تلاش کنید.')
     throw error
   }
 }
